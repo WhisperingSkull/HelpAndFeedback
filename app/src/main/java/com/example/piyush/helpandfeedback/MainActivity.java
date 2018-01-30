c12a45f0db85592fffe7787d131a23049222b7a8
Piyush Kumar <piyushesquire@gmail.com>
2018-01-30 20:43:54 +0530

    Adds files to the the repository

---
+++ app/src/main/java/com/example/piyush/helpandfeedback/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.piyush.helpandfeedback;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    FloatingActionButton compose;
+    FloatingActionButton call;
+    private static final int REQUEST_CALL = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        compose = (FloatingActionButton) findViewById(R.id.compose);
+        call = (FloatingActionButton) findViewById(R.id.call);
+        compose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts("mailto", "feedback@gmail.com", null));
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "User Feedback");
+                startActivity(Intent.createChooser(emailIntent, "Complete action using"));
+            }
+        });
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                makePhoneCall();
+            }
+        });
+    }
+
+    public void makePhoneCall() {
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE}, REQUEST_CALL);
+        } else {
+            startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse("tel:1234567890")));
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CALL) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                makePhoneCall();
+            } else {
+                Toast.makeText(this, "Permission DENIED", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
